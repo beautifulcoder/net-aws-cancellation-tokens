6def09e937635f29e8b5d8939913bacd20ed79da
Camilo Reyes <reyesc@comcast.net>
2023-11-10 22:57:53 -0600

    Initial commit

---
+++ src/NetAwsCancellationTokens/NetAwsCancellationTokensStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace NetAwsCancellationTokens
+{
+    public class NetAwsCancellationTokensStack : Stack
+    {
+        internal NetAwsCancellationTokensStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
